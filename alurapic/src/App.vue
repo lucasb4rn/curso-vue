cf28ec1b7980134529ea4543bf1336193e43d33b
Lucasb4rngit config --global user.email Lucas.b4rn@gmail.comgit config --global user.name Lucasb4rn <Lucas.b4rn@gmail.com>
2019-03-10 04:18:58 -0300

    Inicializando repositorio do curso

---
+++ alurapic/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+
+<div>
+
+
+<div>
+  <h1>{{tituloPagina}}</h1>
+  <li v-for="foto of fotos">
+     <img :src="foto.url" :alt="foto.titulo">
+  </li>
+</div>
+
+
+</div>
+
+
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      tituloPagina: 'Pagina de teste',
+
+     fotos: [
+       {
+       url: 'https://abrilexame.files.wordpress.com/2016/09/size_960_16_9_passarinho1.jpg',
+       titulo: 'passarinho1'
+       },
+      {
+        url: 'https://www.petz.com.br/blog/wp-content/uploads/2017/07/passarinhos02.jpg',
+        titulo: 'passarinho2'
+      }
+     ]
+    }
+  }
+}
+
+</script>
+
+<style>
+
+
+
+</style>
